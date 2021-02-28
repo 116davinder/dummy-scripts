7dacffa9bda60b6b8c164a1aa91128528e868aa6
DAVINDER PAL <dpsangwal@gmail.com>
2021-02-28 12:36:57 +0530

    Update daily-bing-wallpaper.sh

--- daily-bing-wallpaper.sh
+++ daily-bing-wallpaper.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+log_file="/tmp/daily-wallpaper.log"
+
+echo $(date) >> $log_file
+
 bing_region="en-UK"
 download_location="/home/dpal/Pictures"
 bing_url="https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=$bing_region"
@@ -8,11 +12,14 @@ today_wall_url=$(curl -s  $bing_url | jq .url | tr -d '"')
 
 today_wall_name=$(echo $today_wall_url | cut -d "=" -f2 | tr -d '"')
 
-# echo $today_wall_url
-# echo $today_wall_name
+echo $today_wall_url >> $log_file
+echo $today_wall_name >> $log_file
 
 # download the wallpaper
 curl -s -o $download_location/$today_wall_name $today_wall_url
 
-# update the current wallpaper
+echo "update the current wallpaper" >> $log_file
+
 gsettings set org.gnome.desktop.background picture-uri file:///$download_location/$today_wall_name
+
+gsettings set org.gnome.desktop.screensaver picture-uri file:///$download_location/$today_wall_name
