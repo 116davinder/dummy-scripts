8b5d24776515c74fe57d38c433319e0fda360f54
DAVINDER PAL <dpsangwal@gmail.com>
2021-07-17 20:14:44 +0300

    Update daily-wallpaper.ps1

--- daily-wallpaper.ps1
+++ daily-wallpaper.ps1
@@ -1,6 +1,6 @@
 
 Set-Variable -Name "bing_region" -Value "en-UK"
-Set-Variable -Name "download_location" -Value "C:\Users\davin\Downloads"
+Set-Variable -Name "download_location" -Value "C:\Users\davin\Downloads\wallpapers"
 Set-Variable -Name "bing_url" -Value "https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=$bing_region"
 
 $response=Invoke-RestMethod $bing_url
@@ -9,7 +9,13 @@ $today_wall_url=$response.url
 
 $today_wall_name=$today_wall_url.split('=')[1]
 
-Remove-Item $download_location\$today_wall_name
+try {
+    Remove-Item $download_location\$today_wall_name
+}
+catch [ItemNotFoundException] {
+    pass
+}
 
 # download the wallpaper
 Invoke-WebRequest -Uri $today_wall_url -OutFile $download_location\$today_wall_name
+
