59e71a35facb41bafb098ce01568c73114a2a406
DAVINDER PAL <dpsangwal@gmail.com>
2021-07-18 14:54:12 +0300

    Update daily-wallpaper.ps1

--- daily-wallpaper.ps1
+++ daily-wallpaper.ps1
@@ -2,6 +2,7 @@
 Set-Variable -Name "bing_region" -Value "en-UK"
 Set-Variable -Name "download_location" -Value "C:\Users\davin\Downloads\wallpapers"
 Set-Variable -Name "bing_url" -Value "https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=$bing_region"
+Set-Variable -Name "cleanUpDays" -Value 5 
 
 $response=Invoke-RestMethod $bing_url
 
@@ -9,13 +10,11 @@ $today_wall_url=$response.url
 
 $today_wall_name=$today_wall_url.split('=')[1]
 
-try {
-    Remove-Item $download_location\$today_wall_name
-}
-catch [ItemNotFoundException] {
-    pass
-}
+Remove-Item $download_location\$today_wall_name -ErrorAction SilentlyContinue
 
 # download the wallpaper
 Invoke-WebRequest -Uri $today_wall_url -OutFile $download_location\$today_wall_name
 
+Get-ChildItem -Force -Recurse -File -Path $download_location -ErrorAction SilentlyContinue `
+    | Where-Object { $_.CreationTime.Date -lt (Get-Date).Date.AddDays(-$cleanUpDays) } `
+    | Remove-Item -Force
